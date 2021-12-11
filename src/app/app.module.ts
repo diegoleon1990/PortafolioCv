549cbf871bebc2fdfdd89fed58988450e7f61eb9
Diego Leonardi <dleonardi55@gmail.com>
2021-12-11 15:33:56 -0300

    11/12/2021 Subiendo a github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ResumenComponent } from './pages/resumen/resumen.component';
+import { BlogComponent } from './pages/blog/blog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    PortafolioComponent,
+    ResumenComponent,
+    BlogComponent,
+    
   ],
   imports: [
     BrowserModule,
